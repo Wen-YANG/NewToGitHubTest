0e692189075a37dde88e7c68c729d502548f28cf
Wen-YANG <yang.wen@orange.fr>
2017-11-12 23:58:01 +0100

    added other links

--- test.c
+++ test.c
@@ -2,3 +2,12 @@
 //this is the second line
 // this is a very useful link for github commits
 // https://help.github.com/articles/adding-an-existing-project-to-github-using-the-command-line/
+//
+//link : git hub general information
+// https://guides.github.com/activities/hello-world/
+//
+//git hub command line
+// http://product.hubspot.com/blog/git-and-github-tutorial-for-beginners
+//
+// git gui beginner
+// http://matthew-brett.github.io/pydagogue/git_gui_windows.html
