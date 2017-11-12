abecfb7467a22675c19ad46f09604e07433ea809
Wen-YANG <yang.wen@orange.fr>
2017-11-12 23:33:13 +0100

    added the website link

--- test.c
+++ test.c
@@ -1,3 +1,4 @@
 //this is the first line
 //this is the second line
-
+// this is a very useful link for github commits
+// https://help.github.com/articles/adding-an-existing-project-to-github-using-the-command-line/
